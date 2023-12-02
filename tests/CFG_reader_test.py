b7694360edd19f8695689f00ba53911713d2a6ee
biddls <thomasbiddlecombe@gmail.com>
2023-12-02 14:10:30 +0000

    added parsing and cleaning for the CFG reader

--- tests/CFG_reader_test.py
+++ tests/CFG_reader_test.py
@@ -5,3 +5,8 @@ class Test_CFG_reader:
     def test_parsedOpcodes(self):
         reader = CFG_reader.CFG_Reader()
         assert len(reader.parsedOpcodes) > 1
+
+    def test_checkParsedOpcodes(self):
+        reader = CFG_reader.CFG_Reader()
+        assert len(reader.parsedOpcodes[0]) >= 1
+        assert isinstance(reader.parsedOpcodes[0][0], str)
