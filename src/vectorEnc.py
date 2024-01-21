c3cb9c40cbf5e2c188f586811d6664edc3040e4a
Biddls <thomasbiddlecombe@gmail.com>
2024-01-21 21:24:46 +0000

    Integrating LSTM autoend with data, fixxing issues

--- src/vectorEnc.py
+++ src/vectorEnc.py
@@ -1,15 +1,23 @@
 import tokeniser
-
+from vectorEncoding.LSTM_Autoenc import LSTM_AutoEnc_Training
+from tqdm import tqdm
 
 if __name__ == "__main__":
     loader = tokeniser.CFG_Loader()
     data = list()
-    for cfg in loader:
+    count = 0
+    for cfg in tqdm(loader):
         tokens = tokeniser.Tokeniser.preProcessing(cfg)
         vectors = tokeniser.Tokeniser.tokenise(tokens)
 
-        for vector in vectors:
-            data.extend(vector)
+        data.extend(vectors)
+        count += 1
+        if count == 10:
+            break
+
+    trainer = LSTM_AutoEnc_Training(data, 100)
+    trainer.trainEnc(10)
+    out = trainer.model.getVectors(data)
 
 
 
--- src/vectorEncoding/LSTM_Autoenc.py
+++ src/vectorEncoding/LSTM_Autoenc.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 device = torch.device('cuda')
 
 
 class Encoder(nn.Module):
-    def __init__(self, seq_len, n_features, embedding_dim=32):
+    def __init__(self, n_features, embedding_dim):
         super(Encoder, self).__init__()
-        self.seq_len, self.n_features = seq_len, n_features
+        self.n_features = n_features
         self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
         self.rnn1 = nn.LSTM(
             input_size=n_features,
@@ -33,18 +34,18 @@ class Encoder(nn.Module):
         return hidden_n.reshape((x.shape[0], self.embedding_dim))
 
 class Decoder(nn.Module):
-    def __init__(self, seq_len, n_features, input_dim=32):
+    def __init__(self, n_features, input_dim):
         super(Decoder, self).__init__()
-        self.seq_len, self.input_dim = seq_len, input_dim
+        self.input_dim = n_features
         self.hidden_dim, self.n_features = 2 * input_dim, n_features
         self.rnn1 = nn.LSTM(
         input_size=input_dim,
-        hidden_size=input_dim,
+        hidden_size=self.hidden_dim,
         num_layers=1,
         batch_first=True
         )
         self.rnn2 = nn.LSTM(
-        input_size=input_dim,
+        input_size=self.hidden_dim,
         hidden_size=self.hidden_dim,
         num_layers=1,
         batch_first=True
@@ -58,10 +59,10 @@ class Decoder(nn.Module):
 
 
 class LSTMAE(nn.Module):
-    def __init__(self, seq_len, n_features, embedding_dim=32):
+    def __init__(self, n_features, embedding_dim):
         super(LSTMAE, self).__init__()
-        self.encoder = Encoder(seq_len, n_features, embedding_dim).to(device)
-        self.decoder = Decoder(seq_len, n_features, embedding_dim).to(device)
+        self.encoder = Encoder(n_features, embedding_dim).to(device)
+        self.decoder = Decoder(n_features, embedding_dim).to(device)
 
     def forward(self, x):
         x = self.encoder(x)
@@ -71,23 +72,33 @@ class LSTMAE(nn.Module):
     def getVectors(self, x):
         return self.encoder.getVectors(x)
 
-class training:
-    def __init__(self, data, epochs_num):
+
+class LSTM_AutoEnc_Training:
+    def __init__(self, data, embedding_dim):
         train_size = int(0.8 * len(data))
         self.train, self.valid = data[:train_size], data[train_size:]
 
-        self.epochs_num = epochs_num
-        self.model = LSTMAE(data.shape[1], data[2].shape[1], embedding_dim=32).to(device)
+        # todo convert to using a list and no batching and processing each one individually
+        self.train = torch.tensor(self.train).float().to(device)
+        self.valid = torch.tensor(self.valid).float().to(device)
+
+        if isinstance(data, list):
+            width = data[0].shape[0]
+        else:
+            width = data.shape[1]
+
+        self.model = LSTMAE(width, embedding_dim).to(device)
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.1, momentum=0.9)
         self.criterion = nn.MSELoss()
 
-    def trainEnc(self):
+    def trainEnc(self, epochs_num):
+        self.epochs_num = epochs_num
         for epoch in range(self.epochs_num):
             # Training mode
             self.model.train()
             self.optimizer.zero_grad()
 
-            seq_pred = self.model(self.train.to(device))
+            seq_pred = self.model(self.train)
             loss = self.criterion(seq_pred, self.train)
             loss.backward()
             self.optimizer.step()
@@ -96,14 +107,14 @@ class training:
             # Evaluation mode
             self.model.eval()
             with torch.no_grad():
-                seq_pred_valid = self.model(self.valid.to(device))
+                seq_pred_valid = self.model(self.valid)
                 val_loss = self.criterion(seq_pred_valid, self.valid).item()
 
             print(f'Epoch [{epoch+1}/{self.epochs_num}], Train Loss: {train_loss:.4f}, Val Loss: {val_loss:.4f}')
 
 
 if __name__ == "__main__":
-    data = torch.rand((10, 20, 32)).to(device)
-    trainer = training(data, 100)
-    trainer.trainEnc()
+    data = torch.rand((10, 20, 30)).to(device)
+    trainer = LSTM_AutoEnc_Training(data, 32)
+    trainer.trainEnc(100)
     out = trainer.model.getVectors(data)
