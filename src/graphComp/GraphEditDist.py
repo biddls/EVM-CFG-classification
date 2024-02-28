51b3dc30a4475179cf101f6e213e6415b81aaa27
Biddls <thomasbiddlecombe@gmail.com>
2024-02-28 04:27:32 +0000

    Got basic layout of graph labeling class

--- src/graphComp/GraphEditDist.py
+++ src/graphComp/GraphEditDist.py
@@ -1,5 +1,35 @@
 import networkx as nx
 import numpy as np
-import matplotlib.pyplot as plt
+from numpy import typing as npt
+from matplotlib import pyplot as plt
+from CFG_reader import CFG_Reader
+
 # nx.graph_edit_distance
+
+class graphClassification:
+    """
+    Class for labeling graph
+    """
+    CFGs: list[CFG_Reader]
+    pathToTypes: str
+    tf_idf: npt.NDArray
+    average: npt.NDArray
+    lstm: npt.NDArray
+    
+    def __init__(
+        self,
+        CFGs: list[CFG_Reader],
+        pathToTypes: str,
+        tf_idf: npt.NDArray | None = None,
+        average: npt.NDArray | None = None,
+        lstm: npt.NDArray | None = None,
+        ) -> None:
+        self.CFGs = CFGs
+        self.pathToTypes = pathToTypes
+        if tf_idf is not None:
+            self.tf_idf = tf_idf
+        if average is not None:
+            self.average = average
+        if lstm is not None:
+            self.lstm = lstm
 
